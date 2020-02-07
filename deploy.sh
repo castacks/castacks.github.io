5088b823152e4e52a13ccd6926b3abc0f97b0df0
Sourish Ghosh <ragnarok0211@gmail.com>
2020-02-07 15:57:33 -0500

    deploy based on last commit

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 git clone https://github.com/castacks/website-dev airlab-temp
 cd airlab-temp
-git pull origin master
-docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll:4.0 jekyll build
+last_commit=$(git log -1 --format="%at" | xargs -I{} date -d @{} +%s)
+git clone https://github.com/castacks/castacks.github.io _site
 cd _site
-touch .nojekyll
-touch CNAME
-echo "theairlab.org" >> CNAME
-git init
-git remote add origin https://github.com/castacks/castacks.github.io
-git add -A
-git commit -m "Auto deploy"
-git push origin master -f
-cd ../../
-rm -rf airlab-temp
+last_push=$(git log -1 --format="%at" | xargs -I{} date -d @{} +%s)
+if [ $last_commit -ge $last_push ]; then
+  cd ..
+  rm -rf _site
+  docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll:4.0 jekyll build
+  cd _site
+  git init
+  git remote add origin https://github.com/castacks/castacks.github.io
+  touch .nojekyll
+  touch CNAME
+  echo "theairlab.org" >> CNAME
+  git add -A
+  git commit -m "Auto deploy"
+  git push origin master -f
+  cd ../../
+  rm -rf airlab-temp
+  echo "Pushed changes!"
+else
+  cd ../../
+  rm -rf airlab-temp
+  echo "Up to date!"
+fi 
